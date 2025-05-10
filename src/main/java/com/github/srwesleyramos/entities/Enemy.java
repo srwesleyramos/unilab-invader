16ba3a909fffaa181f7897c65a5ed83479172ec2
Wesley Ramos <dev.wesleygabriel@gmail.com>
2025-05-10 14:37:03 -0300

    feat: correcting column and row

--- src/main/java/com/github/srwesleyramos/entities/Enemy.java
+++ src/main/java/com/github/srwesleyramos/entities/Enemy.java
@@ -12,19 +12,19 @@ public class Enemy extends BaseEntity {
     public static final int ENEMY_WIDTH = 20;
     public static final int ENEMY_HEIGHT = 16;
 
-    private final int row, column;
+    private final int column, row;
     private final EnemyType type;
 
-    public Enemy(Frame frame, int column, int row, EnemyType type) {
+    public Enemy(Frame frame, int row, int column, EnemyType type) {
         super(frame, ENEMY_WIDTH, ENEMY_HEIGHT);
 
-        this.row = row;
         this.column = column;
+        this.row = row;
         this.type = type;
 
         this.spawn(
-                82 + (ENEMY_WIDTH * (row - 1)) + (10 * (row - 1)),
-                60 + (ENEMY_HEIGHT * (column - 1)) + (10 * (column - 1))
+                82 + (ENEMY_WIDTH * (column - 1)) + (10 * (column - 1)),
+                60 + (ENEMY_HEIGHT * (row - 1)) + (10 * (row - 1))
         );
     }
 
