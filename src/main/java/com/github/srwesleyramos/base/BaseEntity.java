fdb89be1d6cf0bb9e718044050a68518c54e9f44
Wesley Ramos <dev.wesleygabriel@gmail.com>
2025-05-10 15:15:20 -0300

    feat: keeping x0 and y0 on base entity

--- src/main/java/com/github/srwesleyramos/base/BaseEntity.java
+++ src/main/java/com/github/srwesleyramos/base/BaseEntity.java
@@ -12,6 +12,8 @@ public class BaseEntity extends JLabel {
     private final Frame frame;
     private final int width, height;
 
+    private int x0, y0;
+
     public BaseEntity(Frame frame, int width, int height) {
         this.frame = frame;
         this.width = width;
@@ -19,6 +21,9 @@ public class BaseEntity extends JLabel {
     }
 
     public void spawn(int x0, int y0) {
+        this.x0 = x0;
+        this.y0 = y0;
+
         this.setBounds(x0, y0, width, height);
         this.applySprite();
         this.frame.add(this);
